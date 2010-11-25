7dc270b69732ce8361f32428e99e8a4eb6204979
rafaeluchoa <rafaeluchoa@localhost>
2010-11-24 23:50:03 -0300

    projeto

--- src/main/edu/flf/aps/bancox/application/AbrirConta.java
+++ src/main/edu/flf/aps/bancox/application/AbrirConta.java
@@ -2,7 +2,7 @@ package edu.flf.aps.bancox.application;
 
 import java.util.List;
 
-import edu.flf.aps.bancox.application.vo.AbrirContaVO;
+import edu.flf.aps.bancox.application.vo.AbrirContaDTO;
 import edu.flf.aps.bancox.domain.Agencia;
 import edu.flf.aps.bancox.domain.ContaBancaria;
 import edu.flf.aps.bancox.infrastruture.repositorio.RepositorioAgencia;
@@ -10,7 +10,7 @@ import edu.flf.aps.bancox.infrastruture.repositorio.RepositorioContaBancaria;
 import edu.flf.aps.bancox.infrastruture.repositorio.RepositorioPessoa;
 
 public interface AbrirConta {
-	public ContaBancaria abraConta(AbrirContaVO vo) throws IllegalAccessException;
+	public ContaBancaria abraConta(AbrirContaDTO vo) throws IllegalAccessException;
 	
 	void setRepositorioContaBancaria(RepositorioContaBancaria repositorio);
 	void setRepositorioPessoa(RepositorioPessoa repositorio);
--- src/main/edu/flf/aps/bancox/application/AutenticarCliente.java
+++ src/main/edu/flf/aps/bancox/application/AutenticarCliente.java
@@ -2,6 +2,9 @@ package edu.flf.aps.bancox.application;
 
 import java.util.List;
 
+import edu.flf.aps.bancox.application.vo.AutenticarClienteDTO;
+
 public interface AutenticarCliente {
 	List<Integer[]> gerarTeclasAleatorias();
+	void autenticarCliente(AutenticarClienteDTO vo);
 }
